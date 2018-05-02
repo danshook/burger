bf2d76089de5db8de9043d16744b12ad6c21eb88
Space Mac <spacemac@Daniels-MacBook-Pro.local>
2018-05-02 15:09:40 -0700

    added connection to mysql db.

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,20 @@
 // Set up MySQL connection
 var mysql = require("mysql");
 
-var connection;
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "CuL@8Poos",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("Connected to MySQL with id... " + connection.threadId);
+});
+
+module.exports = connection;
