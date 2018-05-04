c96ca02eaba0110cc01acae31175b4967bca82d7
Space Mac <spacemac@Daniels-MacBook-Pro.local>
2018-05-04 10:03:24 -0700

    adding query functions to orm file.

--- config/orm.js
+++ config/orm.js
@@ -11,6 +11,24 @@ var orm = {
       cb(result);
       console.log("I got my results!");
     });
+  },
+
+  insertOne: function(tableName, cols, vals, cb) {
+    var queryString = "INSERT INTO " + tableName;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += vals.map(val => "?").toString;
+    queryString += ") ";
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
   }
 };
 
